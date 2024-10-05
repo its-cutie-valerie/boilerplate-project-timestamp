85b46299856a5457b7b2ff3d29ea93ecb03d9e1c
Valerie <valernov00@gmail.com>
2024-10-05 12:20:29 +0200

    The code

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// enable CORS (https://en.wikipedia.org/wiki/cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,15 +18,42 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// Timestamp API endpoint
+
+app.get('/api/:date', function(req, res) {
+  let date=new Date (req.params.date);
+
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  if(isInvalidDate(date)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
+app.get('/api', function(req, res) {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+}).on('error', function(err) {
+  if (err.code === 'EADDRINUSE') {
+    console.error('Port 3000 is already in use. Please use a different port.');
+    process.exit(1);
+  }
 });
